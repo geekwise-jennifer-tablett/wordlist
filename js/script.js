2694790c96ce2976695b7bbb5de3f50c028827a2
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-19 17:21:04 +0000

    added input

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var input_element
+
+//create input so users can add words
+input_element = document.createElement('input');
+input_element.setAttribute('id','word_box');
+
+//when webpage loads
+document.addEventListener('DOMContentLoaded',function(){});
